ea8659d2f658438e4ea50ab6e50e80fe43142608
absi2011 <absi2011@126.com>
2026-05-07 02:43:38 +0800

    能力 造成灼燃伤害

---
+++ Scripts/Powers/DealFlamingDamagePower.cs
@@ -0,0 +1,42 @@
+using ArknightsMap.Scripts.Enchantments;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.ValueProps;
+using STS2RitsuLib.Interop.AutoRegistration;
+using STS2RitsuLib.Scaffolding.Content;
+
+namespace ArknightsMap.Scripts.Powers;
+
+[RegisterPower]
+public class DealFlamingDamagePower : ModPowerTemplate
+{
+    // 类型，Buff或Debuff
+    public override PowerType Type => PowerType.Debuff;
+    // 叠加类型，Counter表示可叠加，Single表示不可叠加
+    public override PowerStackType StackType => PowerStackType.Single;
+    protected override IEnumerable<DynamicVar> CanonicalVars => [];
+    protected override IEnumerable<IHoverTip> AdditionalHoverTips => HoverTipFactory.FromPowerWithPowerHoverTips<FlamingDamagePower>();
+
+    // 自定义图标路径。1:1即可。原版游戏大图256x256，小图64x64。
+    public override PowerAssetProfile AssetProfile => new(
+        IconPath: "res://Test/images/powers/test_power.png",
+        BigIconPath: "res://Test/images/powers/test_power.png"
+    );
+    
+    public override Task BeforeDamageReceived(PlayerChoiceContext choiceContext, Creature target, decimal amount, ValueProp props, Creature? dealer, CardModel? cardSource)
+    {
+        if (dealer == Owner)
+        {
+            PowerCmd.Apply<FlamingDamagePower>(choiceContext, target, amount, dealer, null);
+        }
+        return Task.CompletedTask;
+    }
+
+}
