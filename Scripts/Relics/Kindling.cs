f524fffef744a6bedc07769947d50aec0268719b
absi2011 <absi2011@126.com>
2026-05-12 01:51:34 +0800

    Kindling

---
+++ Scripts/Relics/Kindling.cs
@@ -0,0 +1,45 @@
+using Godot;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ArknightsMap.Scripts.Cards;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.Entities.Relics;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.RelicPools;
+using MegaCrit.Sts2.Core.Nodes.Combat;
+using MegaCrit.Sts2.Core.Nodes.Rooms;
+using MegaCrit.Sts2.Core.Rooms;
+using MegaCrit.Sts2.Core.ValueProps;
+using STS2RitsuLib.Interop.AutoRegistration;
+using STS2RitsuLib.Scaffolding.Content;
+
+namespace ArknightsMap.Scripts.Relics;
+
+[RegisterRelic(typeof(SharedRelicPool))]
+public class Kindling : ModRelicTemplate
+{
+	public override RelicRarity Rarity => RelicRarity.Ancient;
+	
+	public override RelicAssetProfile AssetProfile => new(
+		// 小图标（原版85x85）
+		IconPath: $"res://ArknightsMap/images/relics/{GetType().Name}.png",
+		// 轮廓图标（原版85x85）
+		IconOutlinePath: $"res://ArknightsMap/images/relics/{GetType().Name}.png",
+		// 大图标（原版256x256）
+		BigIconPath: $"res://ArknightsMap/images/relics/{GetType().Name}.png"
+	);
+
+	public override async Task AfterObtained()
+	{
+		List<CardPileAddResult> results = new List<CardPileAddResult>();
+		CardModel card = base.Owner.RunState.CreateCard<OverloadStrike>(base.Owner);
+		results.Add(await CardPileCmd.Add(card, PileType.Deck));
+		CardCmd.PreviewCardPileAdd(results, 1f);
+	}
+}
