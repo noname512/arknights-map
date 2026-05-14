c5e04c6523d0cce11cc5ad84458ba7ad83b08182
absi2011 <absi2011@126.com>
2026-05-15 01:29:28 +0800

    塔拉印记

--- Scripts/Powers/FlamingDamagePower.cs
+++ Scripts/Powers/FlamingDamagePower.cs
@@ -1,4 +1,5 @@
 using ArknightsMap.Scripts.Enchantments;
+using Godot;
 using MegaCrit.Sts2.Core.Combat;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Creatures;
@@ -9,19 +10,21 @@ using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Models.Powers;
 using MegaCrit.Sts2.Core.ValueProps;
+using STS2RitsuLib.Combat.HealthBars;
 using STS2RitsuLib.Interop.AutoRegistration;
 using STS2RitsuLib.Scaffolding.Content;
 
 namespace ArknightsMap.Scripts.Powers;
 
 [RegisterPower]
-public class FlamingDamagePower : ModPowerTemplate
+public class FlamingDamagePower : ModPowerTemplate, IHealthBarForecastSource
 {
     // 类型，Buff或Debuff
     public override PowerType Type => PowerType.Debuff;
+
     // 叠加类型，Counter表示可叠加，Single表示不可叠加
     public override PowerStackType StackType => PowerStackType.Counter;
-    protected override IEnumerable<DynamicVar> CanonicalVars => []; // 20 和 8 不应该是写死的数字
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new IntVar("Bound", 20), new IntVar("ExtraDamage", 8)];
     protected override IEnumerable<IHoverTip> AdditionalHoverTips => HoverTipFactory.FromPowerWithPowerHoverTips<VulnerablePower>();
 
     // 自定义图标路径。1:1即可。原版游戏大图256x256，小图64x64。
@@ -32,10 +35,10 @@ public class FlamingDamagePower : ModPowerTemplate
     
     public override async Task AfterTurnEndLate(PlayerChoiceContext choiceContext, CombatSide side)
     {
-        if ((Amount >= 20) && (side != Owner.Side))
+        if ((Amount >= DynamicVars["Bound"].IntValue) && (side != Owner.Side))
         {
             Flash();
-            await CreatureCmd.Damage(choiceContext, Owner, new DamageVar(8, ValueProp.Unpowered), Owner);
+            await CreatureCmd.Damage(choiceContext, Owner, new DamageVar(DynamicVars["ExtraDamage"].IntValue, ValueProp.Unpowered), Owner);
             if (Owner.IsAlive)
             {
                 if ((side == CombatSide.Player) && (!Owner.HasPower<VulnerablePower>()))
@@ -49,5 +52,27 @@ public class FlamingDamagePower : ModPowerTemplate
                 await PowerCmd.Remove(this);
             }
         }
+    }
+    public IEnumerable<HealthBarForecastSegment> GetHealthBarForecastSegments(HealthBarForecastContext context)
+    {
+        if (Amount < DynamicVars["Bound"].IntValue)
+        {
+            return HealthBarForecasts.Single(
+                0, // 展示的数量（例如如果你的能力有2倍效果可以乘2）
+                new Color(0.4f, 0.1f, 0.1f), // 颜色
+                HealthBarForecastGrowthDirection.FromRight // 从左边开始延伸还是右边开始
+                // 0, // 顺序，越大越远离血条边缘，默认0
+                // PreloadManager.Cache.GetMaterial("res://xxx.tres") // 如果需要自定义材质
+            );        
+        }
+
+        int value = Math.Max(0, DynamicVars["ExtraDamage"].IntValue - Owner.Block);
+        return HealthBarForecasts.Single(
+            value, // 展示的数量（例如如果你的能力有2倍效果可以乘2）
+            new Color(0.4f, 0.1f, 0.1f), // 颜色
+            HealthBarForecastGrowthDirection.FromRight // 从左边开始延伸还是右边开始
+            // 0, // 顺序，越大越远离血条边缘，默认0
+            // PreloadManager.Cache.GetMaterial("res://xxx.tres") // 如果需要自定义材质
+        );
     }
 }
---
+++ Scripts/Relics/MarkOfTara.cs
@@ -0,0 +1,86 @@
+using Godot;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ArknightsMap.Scripts.Enchantments;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.Entities.Relics;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.Models.RelicPools;
+using MegaCrit.Sts2.Core.Nodes.Combat;
+using MegaCrit.Sts2.Core.Nodes.Rooms;
+using MegaCrit.Sts2.Core.Rooms;
+using MegaCrit.Sts2.Core.Runs;
+using MegaCrit.Sts2.Core.Saves.Runs;
+using MegaCrit.Sts2.Core.ValueProps;
+using STS2RitsuLib.Interop.AutoRegistration;
+using STS2RitsuLib.Scaffolding.Content;
+
+namespace ArknightsMap.Scripts.Relics;
+
+[RegisterRelic(typeof(SharedRelicPool))]
+public class MarkOfTara : ModRelicTemplate
+{
+	public override RelicRarity Rarity => RelicRarity.Ancient;
+	
+	protected override IEnumerable<DynamicVar> CanonicalVars => [new HealVar(10m), new IntVar("Intangible",3)];
+	protected override IEnumerable<IHoverTip> AdditionalHoverTips => HoverTipFactory.FromPowerWithPowerHoverTips<IntangiblePower>();
+	
+	public override RelicAssetProfile AssetProfile => new(
+		// 小图标（原版85x85）
+		IconPath: $"res://ArknightsMap/images/relics/{GetType().Name}.png",
+		// 轮廓图标（原版85x85）
+		IconOutlinePath: $"res://ArknightsMap/images/relics/{GetType().Name}.png",
+		// 大图标（原版256x256）
+		BigIconPath: $"res://ArknightsMap/images/relics/{GetType().Name}.png"
+	);
+	private bool _wasUsed;
+	public override bool IsUsedUp => _wasUsed;
+	[SavedProperty]
+	public bool WasUsed
+	{
+		get
+		{
+			return _wasUsed;
+		}
+		set
+		{
+			AssertMutable();
+			_wasUsed = value;
+			if (IsUsedUp)
+			{
+				base.Status = RelicStatus.Disabled;
+			}
+		}
+	}
+
+	public override bool ShouldDieLate(Creature creature)
+	{
+		if (creature != Owner.Creature)
+		{
+			return true;
+		}
+		if (WasUsed)
+		{
+			return true;
+		}
+		return false;
+	}
+
+	public override async Task AfterPreventingDeath(Creature creature)
+	{
+		Flash();
+		WasUsed = true;
+		await CreatureCmd.Heal(creature, DynamicVars.Heal.BaseValue);
+		PlayerChoiceContext t = new ThrowingPlayerChoiceContext();
+		if (Owner.Creature.CombatState != null)
+		{
+			await PowerCmd.Apply<IntangiblePower>(t, Owner.Creature, DynamicVars["Intangible"].IntValue, Owner.Creature, null, false);
+		}
+	}	
+}
