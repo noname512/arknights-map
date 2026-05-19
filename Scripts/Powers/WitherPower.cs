171529f99735f4654caa36f3be84b88d3feeae68
absi2011 <absi2011@126.com>
2026-05-20 01:48:54 +0800

    WitherPower

--- Scripts/Powers/WitherPower.cs
+++ Scripts/Powers/WitherPower.cs
@@ -3,6 +3,7 @@ using MegaCrit.Sts2.Core.Combat;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization;
 using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Models.Cards;
 using MegaCrit.Sts2.Core.Models.Powers;
@@ -40,7 +41,7 @@ public class WitherPower : TemporaryStrengthPower
         }
         Flash();
         await PowerCmd.Remove(power);
-        await PowerCmd.Apply<StrengthPower>(choiceContext, power.Owner, (-power.Amount), power.Owner, null);
+        await PowerCmd.Apply<StrengthPower>(choiceContext, power.Owner, power.Amount, power.Owner, null);
     }
     
 }
