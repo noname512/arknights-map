7b5b0be49219b147abffedf19ecccda400b49746
noname512 <zxononame@gmail.com>
2026-06-04 23:24:42 +0800

    深池重甲卫士

---
+++ Scripts/Encounters/Breakout.cs
@@ -0,0 +1,35 @@
+using ArknightsMap.Scripts.Acts;
+using ArknightsMap.Scripts.Monsters;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Rooms;
+using STS2RitsuLib.Interop.AutoRegistration;
+using STS2RitsuLib.Scaffolding.Content;
+
+namespace ArknightsMap.Scripts.Encounters;
+
+[RegisterActEncounter(typeof(Wilds))]
+public class Breakout : AbstractWildsEncounter
+{
+    public override IEnumerable<MonsterModel> AllPossibleMonsters => [ModelDb.Monster<DublinnHeavyDefender>()];
+
+    public override bool IsWeak => false;
+
+    public override EncounterAssetProfile AssetProfile => new(
+        RunHistoryIconPath: $"res://ArknightsMap/images/map/{GetType().Name}History.png",
+        RunHistoryIconOutlinePath: $"res://ArknightsMap/images/map/{GetType().Name}History_outline.png",
+        EncounterScenePath: $"res://ArknightsMap/scenes/encounters/{GetType().Name}.tscn"
+    );
+
+    public override IReadOnlyList<string> Slots => [
+        "first"
+    ];
+
+    public override RoomType RoomType => RoomType.Monster;
+
+    // 如果你的场景太大，可以调整缩放。此外还可以使用 GetCameraOffset 来调整摄像机位置
+    // public override float GetCameraScaling() => 0.8f;
+
+    protected override IReadOnlyList<(MonsterModel, string?)> GenerateMonsters() => [
+        (ModelDb.Monster<DublinnHeavyDefender>().ToMutable(), "first")
+    ];
+}
---
+++ Scripts/Monsters/DublinnHeavyDefender.cs
@@ -0,0 +1,92 @@
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Animation;
+using MegaCrit.Sts2.Core.Bindings.MegaSpine;
+using MegaCrit.Sts2.Core.Entities.Ascension;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Helpers;
+using MegaCrit.Sts2.Core.MonsterMoves.Intents;
+using MegaCrit.Sts2.Core.MonsterMoves.MonsterMoveStateMachine;
+using STS2RitsuLib.Interop.AutoRegistration;
+using STS2RitsuLib.Scaffolding.Content;
+using ArknightsMap.Scripts.Powers;
+using MegaCrit.Sts2.Core.MonsterMoves;
+using MegaCrit.Sts2.Core.Runs;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Models;
+
+namespace ArknightsMap.Scripts.Monsters;
+
+[RegisterMonster]
+public class DublinnHeavyDefender : AbstractWildsMonster
+{
+    public override int MinInitialHp => AscensionHelper.GetValueIfAscension(AscensionLevel.ToughEnemies, 50, 46);
+    public override int MaxInitialHp => AscensionHelper.GetValueIfAscension(AscensionLevel.ToughEnemies, 50, 46);
+    // 怪物场景
+    public override MonsterAssetProfile AssetProfile => new(
+        VisualsScenePath: $"res://ArknightsMap/scenes/monsters/{GetType().Name}.tscn"
+    );
+
+    public override async Task AfterAddedToRoom()
+    {
+        foreach (Creature c in CombatState.GetOpponentsOf(Creature))
+        {
+            PerseverePower p = (PerseverePower)ModelDb.Power<PerseverePower>().ToMutable();
+            p.Target = c;
+            await PowerCmd.Apply(new ThrowingPlayerChoiceContext(), p, Creature, 1m, Creature, null);
+        }
+    }
+
+    protected override MonsterMoveStateMachine GenerateMoveStateMachine()
+    {
+        List<MonsterState> list = new List<MonsterState>();
+        int[] Damage = { 7, 8, 9, 10, 11, 12, 13 };
+        MoveState[] attacks = new MoveState[7];
+        Func<int, MoveState> f = i =>
+        {
+            return new MoveState(
+                "ATTACK" + i,
+                async targets => await DamageCmd.Attack(Damage[i]).FromMonster(this).Execute(null),
+                new SingleAttackIntent(Damage[i])
+            );
+        };
+        for (int i = 0; i < 7; i++)
+        {
+            attacks[i] = f(i);
+            list.Add(attacks[i]);
+        }
+
+        RandomBranchState[] randomStates = new RandomBranchState[7];
+        for (int i = 0; i < 7; i++)
+        {
+            randomStates[i] = new RandomBranchState("RANDOM" + i);
+            for (int j = i; j < 7; j++)
+                randomStates[i].AddBranch(attacks[j], MoveRepeatType.CanRepeatForever);
+            list.Add(randomStates[i]);
+        }
+
+        if (RunManager.Instance.HasAscension(AscensionLevel.ToughEnemies))
+        {
+            for (int i = 0; i < 5; i++) attacks[i].FollowUpState = randomStates[i + 1];
+            for (int i = 5; i < 7; i++) attacks[i].FollowUpState = randomStates[0];
+        }
+        else
+        {
+            for (int i = 0; i < 7; i++) attacks[i].FollowUpState = randomStates[0];
+        }
+
+
+        return new MonsterMoveStateMachine(list, randomStates[0]);
+    }
+
+    public override CreatureAnimator GenerateAnimator(MegaSprite controller)
+    {
+        AnimState idleState = new AnimState("Idle", isLooping: true);
+        AnimState attackState = new AnimState("Attack");
+        AnimState dieState = new AnimState("Die");
+        attackState.NextState = idleState;
+        CreatureAnimator creatureAnimator = new CreatureAnimator(idleState, controller);
+        creatureAnimator.AddAnyState("Attack", attackState);
+        creatureAnimator.AddAnyState("Dead", dieState);
+        return creatureAnimator;
+    }
+}
