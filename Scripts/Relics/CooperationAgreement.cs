0cff1d7fcd698edbaf9bbe68c285605e0e119db6
absi2011 <absi2011@126.com>
2026-06-16 00:29:50 +0800

    合作协议

--- Scripts/Relics/CooperationAgreement.cs
+++ Scripts/Relics/CooperationAgreement.cs
@@ -1,9 +1,11 @@
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Relics;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models.Powers;
 using MegaCrit.Sts2.Core.Models.RelicPools;
+using MegaCrit.Sts2.Core.Saves.Runs;
 using STS2RitsuLib.Interop.AutoRegistration;
 using STS2RitsuLib.Scaffolding.Content;
 
@@ -16,6 +18,31 @@ public class CooperationAgreement : ModRelicTemplate
 
 	protected override IEnumerable<DynamicVar> CanonicalVars => [new GoldVar(50), new IntVar("Amount", 0)];
 
+	protected override IEnumerable<IHoverTip> AdditionalHoverTips =>
+	[
+		HoverTipFactory.FromPower<StrengthPower>(), HoverTipFactory.FromPower<DexterityPower>()
+	];
+	
+	private int _amount;
+	public override bool ShowCounter => true;
+	public override int DisplayAmount => _amount;
+
+	[SavedProperty]
+	private int Amount
+	{
+		get
+		{
+			return _amount;
+		}
+		set
+		{
+			AssertMutable();
+			_amount = value;
+			DynamicVars["Amount"].BaseValue = _amount;
+			InvokeDisplayAmountChanged();
+		}
+	}
+
 	public override RelicAssetProfile AssetProfile => new(
 		// 小图标（原版85x85）
 		IconPath: $"res://ArknightsMap/images/relics/{GetType().Name}.png",
@@ -27,13 +54,13 @@ public class CooperationAgreement : ModRelicTemplate
 
 	public override async Task AfterObtained()
 	{
-		DynamicVars["Amount"].BaseValue = Owner.Gold / DynamicVars.Gold.BaseValue;
+		Amount = (int)(Owner.Gold / DynamicVars.Gold.BaseValue);
 		await PlayerCmd.LoseGold(Owner.Gold, Owner);
 	}
 
 	public override async Task BeforeCombatStart()
 	{
-		await PowerCmd.Apply<StrengthPower>(new ThrowingPlayerChoiceContext(), Owner.Creature, DynamicVars["Amount"].BaseValue, Owner.Creature, null);
-		await PowerCmd.Apply<DexterityPower>(new ThrowingPlayerChoiceContext(), Owner.Creature, DynamicVars["Amount"].BaseValue, Owner.Creature, null);
+		await PowerCmd.Apply<StrengthPower>(new ThrowingPlayerChoiceContext(), Owner.Creature, Amount, Owner.Creature, null);
+		await PowerCmd.Apply<DexterityPower>(new ThrowingPlayerChoiceContext(), Owner.Creature, Amount, Owner.Creature, null);
 	}
 }
