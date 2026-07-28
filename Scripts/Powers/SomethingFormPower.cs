57a8abf0412cbcfed83a7602fc249e1231782425
noname512 <zxononame@gmail.com>
2026-07-29 04:38:57 +0800

    长期契约修复

--- Scripts/Powers/SomethingFormPower.cs
+++ Scripts/Powers/SomethingFormPower.cs
@@ -1,8 +1,9 @@
+using Godot;
 using MegaCrit.Sts2.Core.Commands;
-using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Entities.Players;
 using MegaCrit.Sts2.Core.Entities.Powers;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models;
 using STS2RitsuLib.Interop.AutoRegistration;
@@ -16,7 +17,7 @@ public class SomethingFormPower : ModPowerTemplate
     public override PowerType Type => PowerType.Buff;
     public PowerStackType _stackType = PowerStackType.Single;
     public override PowerStackType StackType => _stackType;
-    protected override IEnumerable<DynamicVar> CanonicalVars => [new StringVar("cardName")];
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new StringVar("CardName")];
     public CardModel? baseCard;
 
     public enum XType
@@ -32,24 +33,34 @@ public class SomethingFormPower : ModPowerTemplate
     public override PowerAssetProfile AssetProfile =>
         new(IconPath: $"res://ArknightsMap/images/powers/{GetType().Name}.png", BigIconPath: $"res://ArknightsMap/images/powers/{GetType().Name}.png");
 
-    public override Task AfterApplied(Creature? applier, CardModel? cardSource)
+    public override LocString Title
     {
-        StringVar stringVar = (StringVar)DynamicVars["cardName"];
-        stringVar.StringValue = baseCard!.Title;
-        return Task.CompletedTask;
+        get
+        {
+            LocString title = new LocString("powers", Id.Entry + ".title");
+            DynamicVars.AddTo(title);
+            return title;
+        }
     }
 
-    public override async Task AfterAutoPrePlayPhaseEntered(PlayerChoiceContext choiceContext, Player player)
+    public void SetBaseCard(CardModel baseCard)
+    {
+        this.baseCard = baseCard;
+        StringVar stringVar = (StringVar)DynamicVars["CardName"];
+        stringVar.StringValue = baseCard.Title;
+    }
+
+    public override async Task AfterAutoPrePlayPhaseEnteredEarly(PlayerChoiceContext choiceContext, Player player)
     {
         if (player == Owner.Player)
         {
-            await CardCmd.AutoPlay(choiceContext, baseCard!, null);
+            await CardCmd.AutoPlay(choiceContext, baseCard!.CreateDupe(player), null);
         }
     }
 
     public override int ModifyXValue(CardModel card, int originalValue)
     {
-        if (card == baseCard && xType != XType.None)
+        if ((card == baseCard || card.CloneOf == baseCard) && xType != XType.None)
             return Amount;
         return originalValue;
     }
