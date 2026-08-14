9eb7a87b96b8cbb45670909505b53f1b83618dfb
absi2011 <absi2011@126.com>
2026-08-14 23:18:38 +0800

    聆音

--- Scripts/Acts/Laterano.cs
+++ Scripts/Acts/Laterano.cs
@@ -56,7 +56,7 @@ public sealed class Laterano : ModActTemplate
         {
             ModelDb.AncientEvent<Paganini>(), // 潘格尼尼
             ModelDb.AncientEvent<Lemuen>(), // 蕾缪安
-            
+            ModelDb.AncientEvent<Gracebearer>(), //聆音
         };
 
     public override IEnumerable<EncounterModel> BossDiscoveryOrder =>
