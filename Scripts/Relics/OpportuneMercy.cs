e9d18bde1c50797f13a2be93751f10d8570ab25f
absi2011 <absi2011@126.com>
2026-08-16 00:36:33 +0800

    趁势怜悯

---
+++ Scripts/Relics/OpportuneMercy.cs
@@ -0,0 +1,87 @@
+using ArknightsMap.Scripts.Enchantments;
+using ArknightsMap.Scripts.Powers;
+using Godot;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.Entities.Relics;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Helpers;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Logging;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.RelicPools;
+using MegaCrit.Sts2.Core.Nodes;
+using MegaCrit.Sts2.Core.Nodes.Vfx;
+using MegaCrit.Sts2.Core.ValueProps;
+using STS2RitsuLib.Interop.AutoRegistration;
+using STS2RitsuLib.Scaffolding.Characters;
+using STS2RitsuLib.Scaffolding.Content;
+
+namespace ArknightsMap.Scripts.Relics;
+
+[RegisterRelic(typeof(SharedRelicPool))]
+public class OpportuneMercy : ModRelicTemplate
+{
+    public override RelicRarity Rarity => RelicRarity.Ancient;
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new EnergyVar(1), new EnergyVar("Energy2", 1)];
+
+    public override RelicAssetProfile AssetProfile =>
+        new(
+            // 小图标（原版85x85）
+            IconPath: $"res://ArknightsMap/images/relics/{GetType().Name}.png",
+            // 轮廓图标（原版85x85）
+            IconOutlinePath: $"res://ArknightsMap/images/relics/{GetType().Name}.png",
+            // 大图标（原版256x256）
+            BigIconPath: $"res://ArknightsMap/images/relics/{GetType().Name}.png"
+        );
+    
+    protected override IEnumerable<IHoverTip> AdditionalHoverTips
+    {
+        get
+        {
+            List<IHoverTip> list = new List<IHoverTip>();
+            list.Add(HoverTipFactory.ForEnergy(this));
+            return list;
+        }
+    }
+    
+    private Dictionary<Creature, int> Damaged = new Dictionary<Creature, int>();
+    
+    public override Task BeforeCombatStart()
+    {
+        Damaged.Clear();
+        return Task.CompletedTask;
+    }
+    
+    public override async Task AfterDamageReceived(PlayerChoiceContext choiceContext, Creature target, DamageResult result, ValueProp props, Creature? dealer, CardModel? cardSource)
+    {
+        if (CombatManager.Instance.IsInProgress && target == Owner.Creature && dealer != null && dealer.Monster != null && result.UnblockedDamage > 0)
+        {
+            if (!Damaged.ContainsKey(dealer))
+            {
+                Damaged.Add(dealer, result.UnblockedDamage);
+            }
+            else
+            {
+                Damaged[dealer] += result.UnblockedDamage;
+            }
+        }
+    }
+
+    public override async Task AfterDeath(PlayerChoiceContext choiceContext, Creature creature, bool wasRemovalPrevented, float deathAnimLength)
+    {
+        if (Damaged.ContainsKey(creature))
+        {
+            int val = Damaged[creature] / 2;
+            if (val > 0)
+            {
+                Flash();
+                await CreatureCmd.Heal(Owner.Creature, val);
+            }
+        }
+    }
+}
